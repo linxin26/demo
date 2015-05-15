b1b30cb2918ed370016479bab48365f0afc06e07
linx <644299146@qq.com>
2015-05-15 18:10:17 +0800

    wait

--- multiThread/src/main/java/co/solinx/demo/multithread/prosumer/Generate.java
+++ multiThread/src/main/java/co/solinx/demo/multithread/prosumer/Generate.java
@@ -3,5 +3,21 @@ package co.solinx.demo.multithread.prosumer;
 /**
  * Created by linx on 2015/5/15.
  */
-public class Generate {
+public class Generate  extends Thread{
+    private Warehouse warehouse;
+
+    public Generate(String name, Warehouse warehouse) {
+        this.setName(name);
+        this.warehouse = warehouse;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            warehouse.production();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
